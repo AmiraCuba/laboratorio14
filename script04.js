92c96f096eb77bad0f748df5bf54d1b172bd7f2c
Amira <bcuba@unsa.edu.pe>
2025-11-03 16:39:55 -0500

    Ejercicio 06

---
+++ script04.js
@@ -0,0 +1,5 @@
+const numero = parseInt(prompt("Ingresa un número:"));
+let cuadrado = Math.pow(numero, 2);
+let cubo = Math.pow(numero, 3);
+let raiz = Math.sqrt(numero);
+console.log(`Cuadrado de ${numero}: ${cuadrado} \nCubo de ${numero}: ${cubo} \nRaíz cuadrada de ${numero}: ${raiz}`);
